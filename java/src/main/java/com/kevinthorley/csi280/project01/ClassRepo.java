eaa0c06dd6a40dbb0c7b655ae0373c4e4859dd80
BryanMartin <bryan.martin@mymail.champlain.edu>
2016-01-28 18:00:00 -0500

    Fixed errors and added some comments.

--- java/src/main/java/com/kevinthorley/csi280/project01/ClassRepo.java
+++ java/src/main/java/com/kevinthorley/csi280/project01/ClassRepo.java
@@ -14,12 +14,16 @@ public class ClassRepo {
     this.sections.add(new Section("csi-280-52", "Open Source Software Development", "2016SP"));
   }
 
-  public Section findById(String id) {
-    // TODO: find a section by its id
+  public Section findById(String id) {      //Finds a course by its ID
+    for(int i = 0; i < sections.size(); i++){
+        if(id.equals(sections.get(i).getId())){
+            return sections.get(i);
+        }
+    }
     return null;
   }
 
   public void addSection(Section section) {
-    // TODO: add a section to the repository
+    sections.add(section);
   }
 }
