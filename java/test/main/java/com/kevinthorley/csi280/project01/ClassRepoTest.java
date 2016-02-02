006026fbb6c2235360a2f004f53a467247962545
ajsebastian <ajsebastian123@gmail.com>
2016-02-01 17:50:29 -0800

    First commit

---
+++ java/test/main/java/com/kevinthorley/csi280/project01/ClassRepoTest.java
@@ -0,0 +1,32 @@
+package com.kevinthorley.csi280.project01;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class ClassRepoTest {
+
+  @Test
+  public void testFindById() {
+    ClassRepo classRepo = new ClassRepo();
+
+    Section mySection = classRepo.findById("csi-280-51");
+
+    assertNotNull(mySection);
+    assertEquals("Open Source Software Development", mySection.getName());
+  }
+
+  @Test
+  public void testAdd() {
+    ClassRepo classRepo = new ClassRepo();
+
+    Section newSection = new Section("csi-180", "Emerging Technologies", "2016SP");
+    classRepo.addSection(newSection);
+
+    Section mySection = classRepo.findById("csi-180");
+
+    assertNotNull(mySection);
+    assertEquals("Emerging Technologies", mySection.getName());
+  }
+}
---
+++ javascript/lib/classrepo.js
@@ -0,0 +1,29 @@
+var ClassRepo = exports.ClassRepo = function() {
+  this.classList = [];
+
+  this.classList.push({
+    id: 'csi-280-51',
+    name: 'Open Source Software Development',
+    semester: '2016SP',
+    location: 'Wick 101',
+    startTime: '17:30',
+    endTime: '18:45'
+  });
+
+  this.classList.push({
+    id: 'csi-280-52',
+    name: 'Open Source Software Development',
+    semester: '2016SP',
+    location: 'Joyce 101',
+    startTime: '19:00',
+    endTime: '20:15'
+  });
+}
+
+ClassRepo.prototype.findById = function(id) {
+  // TODO: find a class by its id
+}
+
+ClassRepo.prototype.add = function(myClass) {
+  // TODO: add a class to the repository
+}
