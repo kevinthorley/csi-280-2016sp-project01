75d4feb7678b159e9a58fcdef0ec9043cec118ef
Patrick Dallarosa <patrick.dallarosa@mymail.champlain.edu>
2016-02-02 15:37:49 -0500

    Updated findById and addSection

--- java/src/main/java/com/kevinthorley/csi280/project01/ClassRepo.java
+++ java/src/main/java/com/kevinthorley/csi280/project01/ClassRepo.java
@@ -15,11 +15,20 @@ public class ClassRepo {
   }
 
   public Section findById(String id) {
-    // TODO: find a section by its id
+          int size = this.sections.size();
+
+      for(int i = 0; i < size; i ++)
+      {
+	  if(this.sections.get(i).getId().equals(id))
+	  {
+	      return this.sections.get(i);
+	  }
+      }  
+
     return null;
   }
 
   public void addSection(Section section) {
-    // TODO: add a section to the repository
+      this.sections.add(section);
   }
 }
