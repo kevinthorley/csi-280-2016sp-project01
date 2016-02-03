e1a471661b9d2f48f9325e5bacf22700f965b692
GuyFletcher <fletcher.hart@mymail.champlain.edu>
2016-02-02 19:32:20 -0500

    Final Version

--- javascript/lib/classrepo.js
+++ javascript/lib/classrepo.js
@@ -22,8 +22,26 @@ var ClassRepo = exports.ClassRepo = function() {
 
 ClassRepo.prototype.findById = function(id) {
   // TODO: find a class by its id
+var i;
+	for (i = 0; i < this.classList.length; i++)
+		{
+		if (this.classList[i].id == id)
+                	{
+                 	   return this.classList[i];
+              		}
+		}	
 }
 
 ClassRepo.prototype.add = function(myClass) {
   // TODO: add a class to the repository
+
+	this.classList.push(
+    {
+        id: myClass.id,
+        name: myClass.name,
+        semester: myClass.semester,
+        location: myClass.location,
+        startTime: myClass.startTime,
+        endTime: myClass.endTime
+    })
 }
